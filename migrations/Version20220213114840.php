5f04e7c9d28bc6e922fa28dff21d6a1a79c279d4
mvchn <movchan@gmail.com>
2022-02-13 17:15:07 +0200

    add migrations

--- migrations/Version20220213114840.php
+++ migrations/Version20220213114840.php
@@ -19,13 +19,12 @@ final class Version20220213114840 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
 
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SCHEMA public');
+
     }
 }
