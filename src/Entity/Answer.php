9049ae9bfd34d91eb29ac155bbefc1bf95d9570a
Roman Movchan <movchan@gmail.com>
2022-02-13 14:14:32 +0200

    renaming database tables (#28)

--- src/Entity/Answer.php
+++ src/Entity/Answer.php
@@ -6,6 +6,7 @@ use App\Repository\AnswerRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: AnswerRepository::class)]
+#[ORM\Table(name: 'app_answers')]
 class Answer
 {
     #[ORM\Id]
