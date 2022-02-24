31514f54f0283447f125797588117b8bc3320478
Roman Movchan <movchan@gmail.com>
2022-02-24 23:26:44 +0200

    add admin test (#50)

--- tests/Controller/AdminControllerTest.php
+++ tests/Controller/AdminControllerTest.php
@@ -4,14 +4,15 @@ namespace App\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class LoginControllerTest extends WebTestCase
+class AdminControllerTest extends WebTestCase
 {
-    public function testLoginSuccess(): void
+    public function testAdminSuccess(): void
     {
         $client = static::createClient();
-        $client->request('GET', '/login');
+        $client->request('GET', '/admin');
         $response = $client->getResponse();
 
         $this->assertResponseIsSuccessful();
+
     }
 }
