5f7ce05cf9e0da947b6b7fddd0a89ec0a7cd8135
Roman Movchan <movchan@gmail.com>
2022-06-28 21:53:37 +0300

    add subscribe test cases (#106)

--- tests/Controller/SubscribeControllerTest.php
+++ tests/Controller/SubscribeControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests\Controller;
 
+use App\Model\Subscribe;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class SubscribeControllerTest extends WebTestCase
@@ -13,14 +14,16 @@ class SubscribeControllerTest extends WebTestCase
 
         $client->request('GET', '/subscribe/add');
 
-        $crawler = $client->submitForm('form.label.submit', [
+         $client->submitForm('form.label.submit', [
             'subscribe[name]' => 'test',
             'subscribe[email]' => 'test@example.com',
             'subscribe[agree]' => true,
             'subscribe[know]' => true,
         ]);
 
-        $this->assertResponseIsSuccessful();
+        $response = $client->getResponse();
+
+        $this->assertTrue($response->isRedirect('/subscribe/unsucess'));
     }
 
     /**
@@ -29,7 +32,7 @@ class SubscribeControllerTest extends WebTestCase
     public function testUriSuccess($uri) : void
     {
         $client = static::createClient();
-        $crawler = $client->request('GET', $uri);
+        $client->request('GET', $uri);
 
         $this->assertResponseIsSuccessful();
     }
