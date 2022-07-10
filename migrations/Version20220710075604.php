4ca175e50282ad37d92a51c4b63219ff1a1e7f8f
Roman Movchan <movchan@gmail.com>
2022-07-10 11:45:47 +0300

    default priority (#112)

---
+++ migrations/Version20220710075604.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220710075604 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE app_topics ALTER priority SET NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE app_topics ALTER priority DROP NOT NULL');
+    }
+}
