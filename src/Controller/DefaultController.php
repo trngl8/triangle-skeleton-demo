80fca3ae583452e10b8e02c34b1a41e1e47f97e2
mvchn <movchan@gmail.com>
2023-01-29 21:57:16 +0200

    default templates fixes

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -30,7 +30,7 @@ class DefaultController
         $this->appTheme = $appTheme;
     }
 
-    public function index() : Response
+    public function default() : Response
     {
         $user = $this->security->getUser();
 
@@ -39,18 +39,20 @@ class DefaultController
             //return new RedirectResponse('/login');
         }
 
+        //TODO: check cookie and forward to the index
+
         $button1 = new LinkToRoute('default_module', 'button.more', 'primary', 'bi bi-1-circle');
         $button2 = new LinkToRoute('default_action', 'button.subscribe', 'outline-primary', 'bi bi-2-square');
         $button3 = new LinkToRoute('default_action', 'button.light', 'light');
 
         $products = $this->productRepository->findBy([], ['id' => 'ASC'], 3, 0);
 
-        $templateName = sprintf('%s.html.twig', $this->appTheme); //format, template environment
+        $templateName = sprintf('%s/%s.html.twig', $this->appTheme, 'default');
 
         try {
             $template = $this->twig->load($templateName);
         } catch (LoaderError $e) {
-            throw new ThemeLayoutNotFoundException("Index template not found");
+            throw new ThemeLayoutNotFoundException("Default template not found");
         }
 
         $content = $template->render([
@@ -63,5 +65,31 @@ class DefaultController
 
         return $response;
 
+    }
+
+    public function index() : Response
+    {
+        $templateName = sprintf('%s/%s.html.twig', $this->appTheme, 'index');
+
+        try {
+            $template = $this->twig->load($templateName);
+        } catch (LoaderError $e) {
+            throw new ThemeLayoutNotFoundException("Index template not found");
+        }
+
+        $products = $this->productRepository->findBy([], ['id' => 'ASC'], 3, 0);
+        $button1 = new LinkToRoute('default_module', 'button.more', 'primary', 'bi bi-1-circle');
+        $button2 = new LinkToRoute('default_action', 'button.subscribe', 'outline-primary', 'bi bi-2-square');
+        $button3 = new LinkToRoute('default_action', 'button.light', 'light');
+
+        $content = $template->render([
+            'buttons' => [$button1, $button2, $button3],
+            'products' => $products,
+        ]);
+
+        $response ??= new Response();
+        $response->setContent($content);
+
+        return $response;
     }
 }
