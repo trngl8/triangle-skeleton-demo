9c181c6e9ce76de5b058319c39cb7c0ee1ecc673
mvchn <movchan@gmail.com>
2023-06-20 20:35:40 +0300

    fix validation

--- src/Model/CalendarOrder.php
+++ src/Model/CalendarOrder.php
@@ -10,9 +10,11 @@ class CalendarOrder
     public string $name;
 
     #[Assert\NotBlank]
+    #[Assert\Email]
     public string $email;
 
     #[Assert\NotBlank]
+    #[Assert\Regex('/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/')]
     public string $phone;
 
     #[Assert\NotBlank]
