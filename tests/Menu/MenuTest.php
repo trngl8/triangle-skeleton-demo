d2973cb9143bb764d9b95c0aa529989243ad64a4
mvchn <movchan@gmail.com>
2023-06-23 13:47:53 +0300

    CI module

--- tests/Menu/MenuTest.php
+++ tests/Menu/MenuTest.php
@@ -4,12 +4,14 @@ namespace App\Tests\Menu;
 
 use App\Menu\Menu;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Routing\RouterInterface;
 
 class MenuTest extends TestCase
 {
     public function testMainMenu(): void
     {
-        $menu = new Menu();
+        $router = $this->createMock(RouterInterface::class);
+        $menu = new Menu($router);
         $result = $menu->getTopMenu(3);
         $this->assertCount(3, $result);
     }
