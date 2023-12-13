eb3f5135e9e97b90ce742c0090f7872fae618110
mvchn <movchan@gmail.com>
2023-12-14 01:25:43 +0200

    show theme, close #225

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Button\LinkToRoute;
 use App\Exception\ThemeLayoutNotFoundException;
 use App\Repository\ProductRepository;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -17,38 +18,29 @@ class DefaultController extends AbstractController
 
     private $twig;
 
-    private $appTheme;
-    private $defaultModule;
+    private $logger;
 
-    public function __construct(ProductRepository $productRepository, Environment $twig,
-        string $appTheme,
-        string $defaultModule,
-    ) {
+    public function __construct(ProductRepository $productRepository, Environment $twig, LoggerInterface $logger) {
         $this->productRepository = $productRepository;
         $this->twig = $twig;
-        $this->appTheme = $appTheme;
-        $this->defaultModule = $defaultModule;
+        $this->logger = $logger;
     }
 
     public function default(Request $request) : Response
     {
-        if ($request->cookies->get('THEME_CHOICE')) {
-            $this->forward('App\Controller\DefaultController::index', [
-                'request' => $request,
-            ]);
+        if ($request->cookies->has('APP_THEME')) {
+            $templateName = sprintf('%s.html.twig', $request->cookies->get('APP_THEME'));
+            try {
+                $template = $this->twig->load($templateName);
+            } catch (LoaderError $e) {
+                $this->forward('App\Controller\DefaultController::index');
+            }
+        } else {
+            $this->forward('App\Controller\DefaultController::index');
         }
 
-        $templateName = sprintf('%s/default.html.twig', $this->appTheme);
-
-        try {
-            $template = $this->twig->load($templateName);
-        } catch (LoaderError $e) {
-            throw new ThemeLayoutNotFoundException("Default template not found");
-        }
-
-        //TODO: check routes exists
         $button1 = new LinkToRoute('login', 'button.more', 'primary', 'bi bi-1-circle');
-        $button2 = new LinkToRoute($this->defaultModule, 'button.subscribe', 'outline-primary', 'bi bi-2-square');
+        $button2 = new LinkToRoute('app_index',  'outline-primary', 'bi bi-2-square');
 
         $products = $this->productRepository->findBy([], ['id' => 'ASC'], 3, 0);
 
@@ -57,37 +49,34 @@ class DefaultController extends AbstractController
             'products' => $products,
         ]);
 
-        $response ??= new Response();
-        $response->setContent($content);
-
-        return $response;
+        return $this->createResponse($content);
     }
 
-    public function index(Request $request) : Response
+    public function index() : Response
     {
-        $templateName = sprintf('%s/index.html.twig', $this->appTheme);
-
         try {
-            $template = $this->twig->load($templateName);
+            $template = $this->twig->load('default.html.twig');
         } catch (LoaderError $e) {
-            throw new ThemeLayoutNotFoundException("Index template not found");
+            $this->logger->error($e->getMessage());
+            throw new ThemeLayoutNotFoundException("Default template not found");
         }
 
         $products = $this->productRepository->findBy([], ['id' => 'ASC'], 3, 0);
 
-        //TODO: check routes exists
         $button1 = new LinkToRoute('default', 'button.more', 'primary', 'bi bi-1-circle');
-        $button2 = new LinkToRoute($this->defaultModule, 'button.subscribe', 'outline-primary', 'bi bi-2-square');
-        $button3 = new LinkToRoute($this->defaultModule, 'button.light', 'light');
+        $button2 = new LinkToRoute('default', 'outline-primary', 'bi bi-2-square');
+        $button3 = new LinkToRoute('default', 'button.light', 'light');
 
         $content = $template->render([
             'buttons' => [$button1, $button2, $button3],
             'products' => $products,
         ]);
 
-        $response ??= new Response();
-        $response->setContent($content);
+        return $this->createResponse($content);
+    }
 
-        return $response;
+    private function createResponse(string $content) : Response
+    {
+        return (new Response())->setContent($content);
     }
 }
