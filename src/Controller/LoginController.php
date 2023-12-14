46699ab05d974fc82ab46c8b58cfc799545b7062
Red Rubber Duck <mvch@triangle.software>
2023-12-14 10:26:01 +0200

    template changes

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -19,7 +19,7 @@ class LoginController extends AbstractController
 
             $lastUsername = $authenticationUtils->getLastUsername();
 
-            return $this->render('login/index.html.twig', [
+            return $this->render('default/login.html.twig', [ // the same is login/index.html.twig
                 'error' => $error,
                 'last_username' => $lastUsername,
             ]);
